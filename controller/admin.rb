d367e34e1674e943ab69a085d1cfc54b5e37ffa6
Michael Fellinger <m.fellinger@gmail.com>
2008-06-03 13:31:21 +0900

    Respect User#is_admin

--- controller/admin.rb
+++ controller/admin.rb
@@ -1,3 +1,10 @@
 class AdminController < Ramaze::Controller
   scaffold_all_models
+
+  helper :user, :aspect
+
+  before_all do
+    p user.is_admin
+    redirect_referrer unless user.is_admin
+  end
 end
